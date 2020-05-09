d0510e45c1032a35ce3cf250e113253e24eee4bd
RKLogixal <rohit.karkhanis@logixal.com>
2020-05-09 18:29:27 +0530

    Change in IE driver

--- src/test/java/com/testcases/driverscripts/Execute_MainScript.java
+++ src/test/java/com/testcases/driverscripts/Execute_MainScript.java
@@ -200,8 +200,8 @@ public class Execute_MainScript {
 				ieOptions.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
 				ieOptions.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, false);
 				//ieOptions.setCapability(InternetExplorerDriver.UNEXPECTED_ALERT_BEHAVIOR, false);
-				ieOptions.setCapability("requireWindowFocus", true);
-				ieOptions.setCapability("ignoreZoomSetting", true);
+				//ieOptions.setCapability("requireWindowFocus", true);
+				//ieOptions.setCapability("ignoreZoomSetting", true);
 				//ieOptions.setCapability("nativeEvents",false);
 				//ieOptions.
 
